2744d880affee382c52df2154137b7ad94b9c41c
Earvin <earvin.tli@gmail.com>
2020-11-10 11:47:30 +0800

    [Heroes] feat: add the dashboard view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent },
 ];
 
