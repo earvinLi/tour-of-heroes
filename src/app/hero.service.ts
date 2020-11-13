3350bcf568031ef51816a511db999a5798e73a4c
Earvin <earvin.tli@gmail.com>
2020-11-13 10:43:26 +0800

    [Heroes] feat: enable to search heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,35 +11,15 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService,
   ) {}
-
-  private heroesUrl = 'api/heroes'; // URL to web api
-  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-
-  /** Log a HeroService message with the MessageService **/
-  private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
-  }
-
-  /**
-    * Handle Http operation that failed.
-    * Let the app continue.
-    @param operation - name of the operation that failed
-    @param result - optional value to return as the observable result
-  */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-      // Let the app keep running by returning an empty result
-      return of(result as T);
-    };
-  }
 
   // GET heroes from the server
   getHeroes(): Observable<Hero[]> {
@@ -84,5 +64,38 @@ export class HeroService {
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero')),
     );
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) { return of([]) };
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', [])),
+    )
+  }
+
+  /** Log a HeroService message with the MessageService **/
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+    * Handle Http operation that failed.
+    * Let the app continue.
+    @param operation - name of the operation that failed
+    @param result - optional value to return as the observable result
+  */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result
+      return of(result as T);
+    };
   }
 }
